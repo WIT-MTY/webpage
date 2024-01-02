a22348b4e6bf147e2bcd084e971f21c170139dce
[anpesinaa] <[anpesina08@gmail.com]>
2024-01-02 00:14:57 -0600

    Hola, es intento para jimena

---
+++ wit-webpage/app/(pages)/serviciosocial/page.tsx
@@ -0,0 +1,29 @@
+
+export default function Home() {
+  return (
+    <main>
+    
+      <img src="" />
+
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div>
+        <table align="center">
+          <tbody>
+          <tr>
+          <td align="left">
+          <p >¿Qué es WitCode?</p>
+          </td>
+          <td> <p> &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; </p> </td>
+          <td>
+            adios
+          </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
+}
--- wit-webpage/app/utils/hooks/useRoutes.tsx
+++ wit-webpage/app/utils/hooks/useRoutes.tsx
@@ -8,7 +8,7 @@ export const useRoutes = () => {
        },
        {
            label: "SERVICIO SOCIAL",
-           href: "/servicio"
+           href: "/serviciosocial"
        },
        {
            label: "PATROCINIOS",
