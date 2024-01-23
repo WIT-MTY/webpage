08e115a064dc3899be43fbeec9577985d6cfdd01
JimenaCarmona2 <jimenagarcia0284@gmail.com>
2024-01-22 20:44:05 -0600

    Layout Member Page

---
+++ wit-webpage/app/(pages)/integrantes/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/app/components/general/Footer";
+
+export default function PageMembers() {
+
+    return (
+        <main>
+            <div className="overflow-scroll">
+
+                <Footer />
+            </div>
+        </main>
+    )
+}
