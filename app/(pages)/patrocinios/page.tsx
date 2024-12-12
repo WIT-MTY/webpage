c0a5bd4acbe8b91e93cc0acd4546792dce95c4f3
DanielaHG <A00838702@tec.mx>
2024-12-12 15:00:32 -0600

    Añadi tarjeta de patrocinadores

--- app/(pages)/patrocinios/page.tsx
+++ app/(pages)/patrocinios/page.tsx
@@ -7,11 +7,28 @@ export default function PageSponsoring() {
         <main>
             <div className="overflow-scroll">
                 <Section>
-                    <div className="my-10 aspect-video relative">
+                    <div className="my-3 aspect-video relative">
                         <InfoHeader title="Patrocinios" description="El grupo estudiantil ha colaborado con diferentes empresas de alcance nacional e internacional para el patrocinio de nuestros proyectos, eventos y servicio social. \ Durante el semestre agosto a diciembre del 2023, tuvimos la oportunidad de colaborar con las siguientes empresas para patrocinar Journey to Internship y WitCode."/>
+                        <h2 className="font-bold text-red-500"> *PONER PATROCINIOS ACTUALES* </h2>
+                    </div>
+                </Section>
+                <Section>
+                    <div className="w-full bg-white rounded-2xl shadow-xl p-8 mt-3 mb-10">
+                        <h1 className="text-center text-purple-700 font-bold"> Nuestros Aliados </h1>
+                        <h2 className="text-center text-lg font-normal mb-5">
+                            !Gracias a las empresas patrocinadoras por hacer posibles los eventos de la comunidad!
+                        </h2>
+                        <div className="flex flex-wrap items-center justify-center gap-8">
+                            <img src="images/Apex-systems.png" alt="Apex Systems logo" className="h-16 md:h-20"/>
+                            <img src="images/atoms-studio.png" alt="Atoms Studio logo" className="h-16 md:h-20"/>
+                            <img src="images/Dairy-Queen.png" alt="Dairy Queen logo" className="h-16 md:h-20"/>
+                            <img src="images/Delicias-importadas.png" alt="Delicias importadas logo" className="h-16 md:h-20"/>
+                            <img src="images/Sigma.png" alt="Sigma logo" className="h-16 md:h-20"/>
+                        </div>
                     </div>
                 </Section>
             </div>
+                <Footer/>
         </main>
     )
 }
