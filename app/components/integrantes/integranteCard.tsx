617708be6fb1f22512849561fb7f2a486a5ff686
Alejandra Coeto <a01285221@tec.mx>
2024-12-19 23:39:37 -0600

    Update image of current integrantes

--- app/components/integrantes/integranteCard.tsx
+++ app/components/integrantes/integranteCard.tsx
@@ -1,9 +1,9 @@
 
 const IntegranteCard = ({ nombre, puesto, fecha, position, image, linkedin }: { nombre: string, puesto: string, fecha?: string, position: string, image: string, linkedin?: string }) => {
     const styeRight = {
-        borderTopLeftRadius: '80rem', 
+        borderTopLeftRadius: '80rem',
         borderTopRightRadius: '9999px',
-        borderBottomLeftRadius: '9999px', 
+        borderBottomLeftRadius: '9999px',
         borderBottomRightRadius: '9999px',
     };
 
@@ -13,7 +13,7 @@ const IntegranteCard = ({ nombre, puesto, fecha, position, image, linkedin }: { 
         borderBottomLeftRadius: '9999px',
         borderBottomRightRadius: '9999px',
     };
-    
+
     const styleCenter = {
         borderTopLeftRadius: '80rem',
         borderTopRightRadius: '80rem',
@@ -24,16 +24,16 @@ const IntegranteCard = ({ nombre, puesto, fecha, position, image, linkedin }: { 
     return (
         <div className="w-40">
             <a href={linkedin} target="_blank" rel="noreferrer">
-            <div style={position === "left" ? styleLeft : position === "right" ? styeRight : styleCenter} className={`bg-violet-400 ` }>
-                <img style={styleCenter} src={image}/>
-            </div>
-            <div className="font-montserrat text-center">
-                <div className="font-bold text-lg py-0 leading-3 pt-2 text-secondary"> {nombre} </div>
-                <div className="font-light text-sm py-0"> {puesto} </div>
-                {fecha && (
-                    <p className="font-light text-xs"> {fecha} </p>
-                )}
-            </div>
+                <div style={position === "left" ? styleLeft : position === "right" ? styeRight : styleCenter} className={`bg-violet-400 `}>
+                    <img className="w-40 h-40 object-contain pt-3" style={styleCenter} src={image} />
+                </div>
+                <div className="font-montserrat text-center">
+                    <div className="font-bold text-lg leading-4 pt-2 pb-2 text-secondary"> {nombre} </div>
+                    <div className="font-light leading-3 text-sm py-0"> {puesto} </div>
+                    {fecha && (
+                        <p className="font-light text-xs"> {fecha} </p>
+                    )}
+                </div>
             </a>
         </div>
     )
