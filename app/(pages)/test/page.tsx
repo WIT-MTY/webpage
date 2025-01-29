e7214b3e90afe8a62a7b201fa313745a4f99872a
Alejandra Coeto <a01285221@tec.mx>
2025-01-29 00:32:28 -0500

    Update examples

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -3,17 +3,9 @@ const Test = () => {
     return (
         <div>
             <h1>Test</h1>
-            <Example />
         </div>
     )
 }
 
-export const Example = () => {
-    return (
-        <div>
-            hola
-        </div>
-    )
-}
 
 export default Test;
