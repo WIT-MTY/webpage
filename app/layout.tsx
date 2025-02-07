87357092f1c6047b8c4c6a0a3f78617984338704
Alejandra Coeto <a01285221@tec.mx>
2025-02-07 11:26:29 -0600

    Update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Head from 'next/head';
 import Nav from './components/Nav/Nav'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,9 +8,6 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Wit',
   description: 'Webpage for Wit',
-  icons: {
-    icon: '/favicon.ico'
-  }
 }
 
 export default function RootLayout({
@@ -21,9 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" >
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body className={inter.className} >
         <Nav>
           <div className='h-full'>
