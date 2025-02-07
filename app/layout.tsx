f79e48b00941dcd32ca1b97780a1f7bd20da5a6f
Alejandra Coeto <a01285221@tec.mx>
2025-02-07 11:13:29 -0600

    Recover imgs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head';
 import Nav from './components/Nav/Nav'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Wit',
   description: 'Webpage for Wit',
+  icons: {
+    icon: '/favicon.ico'
+  }
 }
 
 export default function RootLayout({
@@ -17,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" >
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={inter.className} >
         <Nav>
           <div className='h-full'>
