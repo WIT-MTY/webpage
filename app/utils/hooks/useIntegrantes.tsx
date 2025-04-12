f9616baa567026e8fdd407bbfa48c446b1a32f7b
JimenaCarmona2 <jimenagarcia0284@gmail.com>
2025-04-12 12:48:30 -0600

    update integrantes

--- app/utils/hooks/useIntegrantes.tsx
+++ app/utils/hooks/useIntegrantes.tsx
@@ -1,45 +1,41 @@
 
-
 export const useIntegrantes = () => {
     const integrantes = [
         {
             subtitulo: "AGOSTO 2024 - JUNIO 2025",
             integrantes: [
                 {
-                    nombre: "Darianna Telléz",
+                    nombre: "Darianna Téllez",
                     puesto: "Presidenta",
                     image: "/images/integrantes/24-25/dari.png",
                     row: 1,
                     position: "left",
+                    linkedin: "https://www.linkedin.com/in/darianna-t%C3%A9llez-777849257/"
                 },
                 {
-                    nombre: "Brisma Álvarez",
-                    puesto: "Vicepresidente",
+                    nombre: "Brisma Alvarez",
+                    puesto: "Vicepresidenta",
                     image: "/images/integrantes/24-25/brisma.png",
                     row: 1,
-                    position: "right"
+                    position: "right",
+                    linkedin: "https://www.linkedin.com/in/brisma-alvarez-valdez/"
                 },
                 {
-                    nombre: "Ángela Pesina",
+                    nombre: "Angela Pesina",
                     puesto: "Directora de Responsabilidad Social",
                     image: "/images/integrantes/24-25/angela.png",
                     row: 2,
-                    position: "left"
+                    position: "left",
+                    linkedin: "https://www.linkedin.com/in/angelapesina/"
                 },
                 {
                     nombre: "Jimena Carmona",
                     puesto: "Directora de Marketing",
                     image: "/images/integrantes/24-25/jime.png",
                     row: 2,
-                    position: "center"
+                    position: "center",
+                    linkedin: "https://www.linkedin.com/in/jimenacarmona/"
                 },
-                //{
-                    //nombre: "Fátima Moncada",
-                    //puesto: "Directora de proyectos",
-                    //image: "/images/integrantes/24-25/fatima.png",
-                    //row: 2,
-                  //  position: "center"
-                //},
                 {
                     nombre: "Eunice Peña",
                     puesto: "Directora de Finanzas",
@@ -58,28 +54,31 @@ export const useIntegrantes = () => {
                     image: "/images/integrantes/23-24/Sofi.png",
                     row: 1,
                     position: "left",
-                    linkedin: "https://www.linkedin.com/in/alecoeto/"
+                    linkedin: "https://www.linkedin.com/in/sof%C3%ADa-gigdem-garcia-orta-358378236/"
                 },
                 {
                     nombre: "Alejandra Coeto",
                     puesto: "Vicepresidenta",
                     image: "/images/integrantes/23-24/Ale.png",
                     row: 1,
-                    position: "right"
+                    position: "right",
+                    linkedin: "https://www.linkedin.com/in/alecoeto/"
                 },
                 {
-                    nombre: "Ángela Pesina",
+                    nombre: "Angela Pesina",
                     puesto: "Directora de Responsabilidad Social",
                     image: "/images/integrantes/23-24/Angela.png",
                     row: 2,
-                    position: "left"
+                    position: "left",
+                    linkedin: "https://www.linkedin.com/in/angelapesina/"
                 },
                 {
                     nombre: "Jimena Carmona",
                     puesto: "Directora de Marketing",
                     image: "/images/integrantes/23-24/Jime.png",
                     row: 2,
-                    position: "center"
+                    position: "center",
+                    linkedin: "https://www.linkedin.com/in/jimenacarmona/"
                 },
                 {
                     nombre: "Fernanda Quevedo",
@@ -87,15 +86,17 @@ export const useIntegrantes = () => {
                     fecha: "(febrero-junio 2024)",
                     image: "/images/integrantes/23-24/Fer.png",
                     row: 2,
-                    position: "center"
+                    position: "center",
+                    linkedin: "https://www.linkedin.com/in/fernanda-quevedo-593415284/"
                 },
                 {
-                    nombre: "Darianna Telléz",
+                    nombre: "Darianna Téllez",
                     puesto: "Directora de Finanzas",
                     image: "/images/integrantes/23-24/Dari.png",
                     fecha: "(febrero-junio 2024)",
                     row: 2,
-                    position: "right"
+                    position: "right",
+                    linkedin: "https://www.linkedin.com/in/darianna-t%C3%A9llez-777849257/"
                 },
                 {
                     nombre: "Marcela Jiménez",
@@ -103,7 +104,8 @@ export const useIntegrantes = () => {
                     fecha: "(agosto-diciembre 2023)",
                     image: "/images/integrantes/23-24/Marce.png",
                     row: 3,
-                    position: "left"
+                    position: "left",
+                    linkedin: "https://www.linkedin.com/in/marcela-jimenez-penarrieta/"
                 },
                 {
                     nombre: "Ariadna Noli",
