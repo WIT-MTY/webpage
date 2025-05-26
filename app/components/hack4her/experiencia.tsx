8c70a0c3b7585c91503c34a41f80ef1cb985791c
DanielaHG <A00838702@tec.mx>
2025-05-26 17:33:19 -0600

    refactor: changed experience arrow color

--- app/components/hack4her/experiencia.tsx
+++ app/components/hack4her/experiencia.tsx
@@ -35,7 +35,7 @@ const ExpLetrero = () => {
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            className="w-10 h-10 text-black"
+                            className="w-10 h-10 text-white"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
