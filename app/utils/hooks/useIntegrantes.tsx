db7d308b34c9bcc5590413fb8448bdde555ed248
GalileaRestrepo <a01178273@tec.mx>
2025-12-16 00:54:35 -0600

    actualizado para la mesa agosto 2025 - junio 2026

--- app/utils/hooks/useIntegrantes.tsx
+++ app/utils/hooks/useIntegrantes.tsx
@@ -1,7 +1,69 @@
-
-
 export const useIntegrantes = () => {
     const integrantes = [
+        {
+            subtitulo: "AGOSTO 2025 - JUNIO 2026",
+            integrantes: [
+                {
+                    nombre: "Nicole Arámbula",
+                    puesto: "Presidenta",
+                    image: "/images/integrantes/25-26/nicole.png",
+                    row: 1,
+                    position: "left",
+                },
+                {
+                    nombre: "Mariel García",
+                    puesto: "Vicepresidenta",
+                    image: "/images/integrantes/25-26/mariel.png",
+                    row: 1,
+                    position: "right",
+                },
+
+                {
+                    nombre: "Karen Estrada",
+                    puesto: "Directora de Marketing",
+                    image: "/images/integrantes/25-26/karen.png",
+                    row: 2,
+                    position: "left",
+                },
+                {
+                    nombre: "Andrea de la Peña",
+                    puesto: "Directora de Finanzas",
+                    image: "/images/integrantes/25-26/andrea.png",
+                    row: 2,
+                    position: "center",
+                },
+                {
+                    nombre: "Carolina Cruz",
+                    puesto: "Directora de Proyectos",
+                    image: "/images/integrantes/25-26/carolina.png",
+                    row: 2,
+                    position: "right",
+                },
+
+                {
+                    nombre: "María Fernanda Cuevas",
+                    puesto: "Directora de Responsabilidad Social",
+                    image: "/images/integrantes/25-26/mafer.png",
+                    row: 3,
+                    position: "left",
+                },
+                {
+                    nombre: "Azereth Gutiérrez",
+                    puesto: "Directora de Vinculación",
+                    image: "/images/integrantes/25-26/azereth.png",
+                    row: 3,
+                    position: "center",
+                },
+                {
+                    nombre: "Fátima Castillo",
+                    puesto: "Directora de Software",
+                    image: "/images/integrantes/25-26/fatima.png",
+                    row: 3,
+                    position: "right",
+                },
+            ],
+        },
+
         {
             subtitulo: "AGOSTO 2024 - JUNIO 2025",
             integrantes: [
@@ -11,44 +73,42 @@ export const useIntegrantes = () => {
                     image: "/images/integrantes/24-25/dari.png",
                     row: 1,
                     position: "left",
+                    linkedin: "https://www.linkedin.com/in/darianna-t%C3%A9llez-777849257/",
                 },
                 {
                     nombre: "Brisma Álvarez",
                     puesto: "Vicepresidente",
                     image: "/images/integrantes/24-25/brisma.png",
                     row: 1,
-                    position: "right"
+                    position: "right",
+                    linkedin: "https://www.linkedin.com/in/brisma-alvarez-valdez/",
                 },
                 {
                     nombre: "Ángela Pesina",
                     puesto: "Directora de Responsabilidad Social",
                     image: "/images/integrantes/24-25/angela.png",
                     row: 2,
-                    position: "left"
+                    position: "left",
+                    linkedin: "https://www.linkedin.com/in/angelapesina/",
                 },
                 {
                     nombre: "Jimena Carmona",
                     puesto: "Directora de Marketing",
                     image: "/images/integrantes/24-25/jime.png",
                     row: 2,
-                    position: "center"
-                },
-                {
-                    nombre: "Fátima Moncada",
-                    puesto: "Directora de proyectos",
-                    image: "/images/integrantes/24-25/fatima.png",
-                    row: 2,
-                    position: "center"
+                    position: "center",
+                    linkedin: "https://www.linkedin.com/in/jimenacarmona/",
                 },
                 {
                     nombre: "Eunice Peña",
                     puesto: "Directora de Finanzas",
                     image: "/images/integrantes/24-25/eunice.png",
                     row: 2,
-                    position: "right"
-                }
-            ]
+                    position: "right",
+                },
+            ],
         },
+
         {
             subtitulo: "AGOSTO 2023 - JUNIO 2024",
             integrantes: [
@@ -58,28 +118,31 @@ export const useIntegrantes = () => {
                     image: "/images/integrantes/23-24/Sofi.png",
                     row: 1,
                     position: "left",
-                    linkedin: "https://www.linkedin.com/in/alecoeto/"
+                    linkedin: "https://www.linkedin.com/in/sof%C3%ADa-gigdem-garcia-orta-358378236/",
                 },
                 {
                     nombre: "Alejandra Coeto",
                     puesto: "Vicepresidenta",
                     image: "/images/integrantes/23-24/Ale.png",
                     row: 1,
-                    position: "right"
+                    position: "right",
+                    linkedin: "https://www.linkedin.com/in/alecoeto/",
                 },
                 {
                     nombre: "Ángela Pesina",
                     puesto: "Directora de Responsabilidad Social",
                     image: "/images/integrantes/23-24/Angela.png",
                     row: 2,
-                    position: "left"
+                    position: "left",
+                    linkedin: "https://www.linkedin.com/in/angelapesina/",
                 },
                 {
                     nombre: "Jimena Carmona",
                     puesto: "Directora de Marketing",
                     image: "/images/integrantes/23-24/Jime.png",
                     row: 2,
-                    position: "center"
+                    position: "center",
+                    linkedin: "https://www.linkedin.com/in/jimenacarmona/",
                 },
                 {
                     nombre: "Fernanda Quevedo",
@@ -87,15 +150,17 @@ export const useIntegrantes = () => {
                     fecha: "(febrero-junio 2024)",
                     image: "/images/integrantes/23-24/Fer.png",
                     row: 2,
-                    position: "center"
+                    position: "center",
+                    linkedin: "https://www.linkedin.com/in/fernanda-quevedo-593415284/",
                 },
                 {
                     nombre: "Darianna Telléz",
                     puesto: "Directora de Finanzas",
-                    image: "/images/integrantes/23-24/Dari.png",
                     fecha: "(febrero-junio 2024)",
+                    image: "/images/integrantes/23-24/Dari.png",
                     row: 2,
-                    position: "right"
+                    position: "right",
+                    linkedin: "https://www.linkedin.com/in/darianna-t%C3%A9llez-777849257/",
                 },
                 {
                     nombre: "Marcela Jiménez",
@@ -103,7 +168,8 @@ export const useIntegrantes = () => {
                     fecha: "(agosto-diciembre 2023)",
                     image: "/images/integrantes/23-24/Marce.png",
                     row: 3,
-                    position: "left"
+                    position: "left",
+                    linkedin: "https://www.linkedin.com/in/marcela-jimenez-penarrieta/",
                 },
                 {
                     nombre: "Ariadna Noli",
@@ -111,10 +177,11 @@ export const useIntegrantes = () => {
                     fecha: "(agosto-diciembre 2023)",
                     image: "/images/integrantes/23-24/Ari.png",
                     row: 3,
-                    position: "right"
-                }
-            ]
-        }
-    ]
+                    position: "right",
+                },
+            ],
+        },
+    ];
+
     return integrantes;
-}
+};
